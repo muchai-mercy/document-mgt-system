aafe2724926d606bad8b98f83b575313b3eb16ce
Mercy-Muchai <mercy.muchai@andela.com>
2017-06-05 18:11:19 +0300

    [chore] set up project

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require ('express');
+const logger = require ('morgan');
+const bodyParser = require ('body-parser');
+
+const app = express();
+
+app.use(logger('dev'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('*' , (req, res) => res.status(200).send({
+  message: "Let's get this started!"
+}))
+
+module.exports = app;
+
