414cae1ad11e2bf0c41d37ecbbd87aa1454f200d
Mercy-Muchai <mercy.muchai@andela.com>
2017-06-09 11:06:36 +0300

    l

--- server/controllers/document.js
+++ server/controllers/document.js
@@ -4,12 +4,12 @@ const Users = db.Users;
 
 module.exports = {
   create(req, res) {
-    return (Document
+    return Document
       .create({
         title: req.body.title,
         content: req.body.content,
         userId: req.body.userId,
-      }))
+      })
       .then(document => res.status(201).send(document))
       .catch(error => res.status(400).send(error));
   },
