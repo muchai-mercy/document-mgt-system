29ce7d2b005ccef0c75af1c37ebc46e3d580b8f5
Mercy-Muchai <mercy.muchai@andela.com>
2017-06-18 15:19:03 +0300

    [feature #147325519] add input points to the forms

--- client/components/Documents/DocumentList.jsx
+++ client/components/Documents/DocumentList.jsx
@@ -5,18 +5,27 @@ const DocumentList = ({ document }) => {
   return (
     <table className="table">
       <thead>
-      <tr>
-        <th>Title</th>
-        <th>Content</th>
-         <th>Category</th>
-      </tr>
+        <tr>
+          <th>Title</th>
+          <th>Content</th>
+          <th>Category</th>
+        </tr>
       </thead>
       <tbody>
         {document ? document.map(document =>
 
-         <DocumentsListRow key={document.id} document={document}/>
-        ) : <span/>}
-        </tbody>
+          <DocumentsListRow key={document.id} document={document} />
+        ) : <span />}
+        <ul className="pagination">
+          <li className="disabled"><a href="#!"><i className="material-icons">chevron_left</i></a></li>
+          <li className="active"><a href="#!">1</a></li>
+          <li className="waves-effect"><a href="#!">2</a></li>
+          <li className="waves-effect"><a href="#!">3</a></li>
+          <li className="waves-effect"><a href="#!">4</a></li>
+          <li className="waves-effect"><a href="#!">5</a></li>
+          <li className="waves-effect"><a href="#!"><i className="material-icons">chevron_right</i></a></li>
+        </ul>
+      </tbody>
     </table>
   );
 };
--- client/components/Users/UsersList.jsx
+++ client/components/Users/UsersList.jsx
@@ -5,19 +5,19 @@ const UserList = ({ user }) => {
   return (
     <table className="table">
       <thead>
-      <tr>
-        <th>Username</th>
-        <th>Email</th>
-         <th>First Name</th>
+        <tr>
+          <th>First Name</th>
           <th>Last Name</th>
-      </tr>
+          <th>Username</th>
+          <th>Email</th>
+        </tr>
       </thead>
       <tbody>
         {user ? user.map(user =>
 
-         <UsersListRow key={user.id} user={user}/>
-        ) : <span/>}
-        </tbody>
+          <UsersListRow key={user.id} user={user} />
+        ) : <span />}
+      </tbody>
     </table>
   );
 };
